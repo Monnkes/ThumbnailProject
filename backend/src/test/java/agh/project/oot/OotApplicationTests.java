0064b768dbf29d26ec9837c180fb166b6b060b57
Mateusz Sacha <mateuszsacha@student.agh.edu.pl>
2024-12-07 16:25:04 +0100

    Pull request #3: Initialize project
    Merge in TO2024/mi-pt-1500-jelenie from Initialize-project to Milestone_1
    
    Squashed commit of the following:
    
    commit 50faf8116cab549555fe267804d29c19cc4e51d9
    Merge: 4220a0a bad48fd
    Author: Łukasz Kluza <kluza@student.agh.edu.pl>
    Date:   Sat Dec 7 16:22:43 2024 +0100
    
        Pull request #4: Initialize frontend
        
        Merge in TO2024/mi-pt-1500-jelenie from Initialize-frontend to Initialize-project
        
        * commit 'bad48fd706ebef94db7db6cae39428cd8f9d8b2d':
          Initialize backend
    
    commit bad48fd706ebef94db7db6cae39428cd8f9d8b2d
    Author: Lukasz <lukasz.9@o2.pl>
    Date:   Fri Dec 6 09:57:28 2024 +0100
    
        Initialize backend
        
        Moved module oot to parent module - jelenie, so we can add second module later, alse renemed module oot to Backend
        
        renamed Backend to backend bc module names should not contain capital letters and added frontend module to jelenie module
    
    commit 4220a0a9db14f5e1551d1c77baefd16de6af6f60
    Author: Lukasz <lukasz.9@o2.pl>
    Date:   Fri Dec 6 09:57:28 2024 +0100
    
        Initialize backend

---
+++ backend/src/test/java/agh/project/oot/OotApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.project.oot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
